8aa1a5e29437ee12889c270b6c0ccc4808549165
israelysmisaac <israelysmisaac@gmail.com>
2024-12-26 16:21:28 +0530

    mongodb integration

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,9 +1,16 @@
 // Import the 'express' module along with 'Request' and 'Response' types from express
 import express, { Request, Response } from 'express';
+import { courseList } from '../services/course/list-course';
+import { connectToDatabase } from '../shared/database';
+import { CreateCourse } from '../services/course/create-course';
+
+async ()=>{
+  await connectToDatabase()
+}
 
 // Create an Express application
 const app = express();
-
+app.use(express.json())
 // Specify the port number for the server
 const port: number = 3000;
 
@@ -11,6 +18,22 @@ const port: number = 3000;
 app.get('/', (req: Request, res: Response) => {
   // Send a response to the client
   res.send('Hello, TypeScript + Node.js + Express!');
+});
+
+app.get('/courses', (req: Request, res: Response) => {
+  let result = courseList()
+  // Send a response to the client
+  res.send(result);
+});
+
+app.post('/course', async (req: Request, res: Response) => {
+  try {
+    let result = await CreateCourse(req.body);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send('Something broke!: '+ error)
+  }
+  
 });
 
 // Start the server and listen on the specified port
---
+++ src/shared/database.ts
@@ -0,0 +1,25 @@
+import { Db, MongoClient } from 'mongodb'
+
+const MONGODB_URI = "mongodb+srv://host/database?retryWrites=true&w=majority";
+// Once we connect to the database once, we'll store that connection and reuse it so that we don't have to connect to the database on every request.
+export let cachedDb:Db|null = null;
+
+export function connectToDatabase(): Promise<Db> {
+    return new Promise(async (resolve, reject)=> {
+        if (cachedDb) {
+            resolve(cachedDb);
+          }
+          try {
+              // Connect to our MongoDB database hosted on MongoDB Atlas
+              const client = await MongoClient.connect(MONGODB_URI);
+              // Specify which database we want to use
+              const db = await client.db("test-demo");
+              cachedDb = db;
+              resolve(db);
+          } catch (error) {
+              console.log("DB connection Failed", error);
+              reject(error);
+          }
+    })
+    
+}
